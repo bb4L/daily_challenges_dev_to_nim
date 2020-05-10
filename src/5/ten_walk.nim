f06ab41638b3ba87fd31029508b6064a1f23a1f3
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-10 10:21:11 +0200

    Add solution to #5

---
+++ src/5/ten_walk.nim
@@ -0,0 +1,13 @@
+import strutils, math
+
+proc walk_generator*(time:int):seq[string] =
+    if time<4 or floorMod(time,2)!=0:
+        raise newException(ValueError, "Value has to be even and >4")
+    let repeat_count = toInt((toFloat(time) - 4) / 2)
+    result = ("n,e," & "e,".repeat(repeat_count) & "s,w" & ",w".repeat(repeat_count)).split(',')
+
+when isMainModule:
+    echo "4"
+    echo walk_generator(4)
+    echo "8"
+    echo walk_generator(8)
