bd93656b7e9d4fd8f93a4cf2a4b68b9a2f81807c
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-10 17:29:49 +0200

    Add solution for #10

---
+++ src/10/calculate.nim
@@ -0,0 +1,35 @@
+import strutils, system
+
+
+const search_values = @["*", "/", "-","+"]
+
+proc calculate*(data:string):float =
+    var tokens = data.split(" ")
+
+    var lookup_idx = 0
+
+    while len(tokens)>1:
+        var idx = tokens.find(search_values[lookup_idx])
+        while idx > 0:
+            var value = 0.0
+
+            if lookup_idx == 0:
+                value = parseFloat(tokens[idx-1]) * parseFloat(tokens[idx+1])
+            elif lookup_idx == 1:
+                value = parseFloat(tokens[idx-1]) / parseFloat(tokens[idx+1])
+            elif lookup_idx == 2:
+                value = parseFloat(tokens[idx-1]) - parseFloat(tokens[idx+1])
+            elif lookup_idx == 3:
+                value = parseFloat(tokens[idx-1]) + parseFloat(tokens[idx+1])
+
+            tokens[idx-1] = $(value)
+            tokens.del(idx+1)
+            tokens.del(idx)
+
+            idx = tokens.find(search_values[lookup_idx])
+
+        if lookup_idx < len(search_values)-1:
+            inc(lookup_idx)
+
+    echo tokens
+    result = parseFloat(tokens[0])
