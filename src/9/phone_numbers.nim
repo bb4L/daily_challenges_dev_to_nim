83d5e84594c915729cb6f5e5671a65ae21ff731c
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-10 13:46:50 +0200

    Add solution to #9

---
+++ src/9/phone_numbers.nim
@@ -0,0 +1,10 @@
+import strutils
+
+proc phone_number(numbers:seq[int]):string=
+    if len(numbers) < 10:
+        raise newException(ValueError, "Less than 10 digits provided")
+    result = "(" & numbers[0..2].join("") & ") " & numbers[3..6].join("") & "-" & numbers[6..9].join("")
+
+when isMainModule:
+    echo ""
+    echo phone_number(@[1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
