18807299ea0d2b2410efce696c8f012c0b76e596
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-11 21:33:37 +0200

    Add solution for #16

---
+++ src/16/bus.nim
@@ -0,0 +1,9 @@
+import sequtils
+
+type
+    BusStop = tuple
+        leaving: int
+        entering:int
+
+proc get_people_in_bus*(data: seq[BusStop]):int =
+    result = data.foldl(a - b.leaving + b.entering, 0)
