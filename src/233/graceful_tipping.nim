afce47e3d8ac48f40995edfcae85c0c7c1cc5ce4
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-11 17:31:40 +0200

    Add solution for #233

---
+++ src/233/graceful_tipping.nim
@@ -0,0 +1,15 @@
+import math
+
+proc gracefulTipping*(rest_bill:float):float=
+    result = ceil(rest_bill*1.15)
+    
+    if result < 10:
+        return result
+    
+    let exp = floor(log10(result))-1
+   
+    let mod_op = 5 * pow(10.0,exp)
+
+    if floorMod(result,mod_op)>0:
+        result += (mod_op - floorMod(result,mod_op))
+        
