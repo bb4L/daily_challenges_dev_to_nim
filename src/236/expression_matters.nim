84b59ef6c503bf612717fa86af1f4f7b2022e11f
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-13 18:26:09 +0200

    Add solution for #236

---
+++ src/236/expression_matters.nim
@@ -0,0 +1,14 @@
+import sequtils
+
+proc expression_matters*(input_numbers:seq[int]):int =
+    if input_numbers.contains(1):
+        if input_numbers[0] > input_numbers[2]:
+            result = (input_numbers[1]+input_numbers[2]) * input_numbers[0]
+        elif input_numbers[0] < input_numbers[2]:
+            result = (input_numbers[1] + input_numbers[0]) * input_numbers[2]
+        elif input_numbers[0] == input_numbers[1]:
+            result = input_numbers.foldl(a+b)
+        else:
+            result = (input_numbers[1] + input_numbers[0]) * input_numbers[2]
+    else:
+        result = input_numbers.foldl(a*b)
