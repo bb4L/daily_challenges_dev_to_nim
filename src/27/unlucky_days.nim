38d3d89691ac91a013e2cd124786b481a989dbc4
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-14 22:35:58 +0200

    Add solution for #27

---
+++ src/27/unlucky_days.nim
@@ -0,0 +1,9 @@
+import times, sequtils
+
+proc unlucky_days*(year:int): int = 
+    len(Month.mapIt(it).map(proc(x:Month):WeekDay=getDayOfWeek(13, x, year)).filter(proc(x:WeekDay):bool= x==WeekDay.dFri))
+
+    # verbose alternative
+    # for m in Month:
+        # if WeekDay.dFri == getDayOfWeek(13, m, year):
+            # inc(result)
