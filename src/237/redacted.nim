4a26b4b14ca245c2760dcdcfebd84c6bc8414469
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-14 18:10:18 +0200

    Add solution for #237

---
+++ src/237/redacted.nim
@@ -0,0 +1,22 @@
+proc checkDocuments*(filename1:string, filename2:string):bool =
+    var lines_from2 = newSeq[string]()
+    for line in lines filename2:
+        lines_from2.add(line.string)
+
+    var idx = 0
+    for line in lines filename1:
+        if idx > len(lines_from2) or  len(line)!=len(lines_from2[idx]):
+            return false
+
+        var c_count = 0
+        for c in line:
+            if c != 'X' and c != lines_from2[idx][c_count]:
+                return false
+            inc(c_count)
+        
+        inc(idx)
+
+    if len(lines_from2) != idx:
+        return false
+
+    result = true
