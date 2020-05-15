feb3dd75d9c05082fbe3406858f7b17c16d06f09
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-15 16:25:54 +0200

    Improve #36

--- src/36/runningPace_test.nim
+++ src/36/runningPace_test.nim
@@ -5,3 +5,5 @@ suite "test runningPace":
         check(runningPace(3, "8:00") == "02:40")
     test "2":
         check(runningPace(5.3, "50:25") == "09:30")
+    test "3":
+        check(runningPace(176, "50:25") == "00:17")
