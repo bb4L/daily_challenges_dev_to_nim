0212abdad7ec25feaf60c7831232476720a75c39
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-15 20:34:06 +0200

    Add solution for #41

---
+++ src/41/greed.nim
@@ -0,0 +1,16 @@
+import tables
+
+const triplets = {1:1000, 6:600, 5:500, 4:400, 3:300, 2:200}.toTable()
+const singles = {1:100, 5:50,}.toTable()
+
+proc greed*(dices:seq[int]):int =
+    var counts = dices.toCountTable()
+
+    for k,v in counts.pairs():
+        let v3 =  v div 3 
+
+        if v3 > 0:
+            result += v3 * triplets.getOrDefault(k, 0)
+        
+        if k in singles:
+            result += (v - 3 * v3) * singles.getOrDefault(k, 0)
---
+++ src/41/greed_test.nim
@@ -0,0 +1,11 @@
+import unittest, greed
+
+suite "test greed":
+    test "5 1 3 4 1":
+        check(greed(@[5, 1, 3, 4, 1]) == 250)
+
+    test "1 1 1 3 1":
+        check(greed(@[1, 1, 1, 3, 1]) == 1100)
+    
+    test "2 4 4 5 4 ":
+        check(greed(@[2, 4, 4, 5, 4 ]) == 450)
