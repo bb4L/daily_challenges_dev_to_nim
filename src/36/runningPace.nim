46fd47614ac29456da39eab5948ff7e9fbe3db15
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-15 16:22:23 +0200

    Add solution for #36

---
+++ src/36/runningPace.nim
@@ -0,0 +1,9 @@
+import strutils, math, strformat
+
+proc runningPace*(distance:float, time:string): string =
+    let time_str = time.split(":")
+    let time_seconds = 60*parseInt(time_str[0]) + parseInt(time_str[1])
+    let seconds_per_km = floor(time_seconds.toFloat() / distance).toInt()
+
+    if seconds_per_km > 60:
+        return fmt"{seconds_per_km div 60:02}:{seconds_per_km mod 60:02}"
---
+++ src/36/runningPace_test.nim
@@ -0,0 +1,7 @@
+import unittest, runningPace
+
+suite "test runningPace":
+    test "1":
+        check(runningPace(3, "8:00") == "02:40")
+    test "2":
+        check(runningPace(5.3, "50:25") == "09:30")
