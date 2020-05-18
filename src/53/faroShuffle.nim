a6d380347ed6d5811ab72a966af6914535e7affb
bb4L <39266013+bb4L@users.noreply.github.com>
2020-05-18 18:35:47 +0200

    Add solution for #53

---
+++ src/53/faroShuffle.nim
@@ -0,0 +1,8 @@
+proc faroShuffle*(data:seq[string]):seq[string] =
+    let size = len(data)
+    if size mod 2 != 0:
+        return @[]
+    
+    for i in 0 .. (size div 2)-1:
+        result.add(data[i])
+        result.add(data[i+(size div 2)])
