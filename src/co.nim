6c7fe36088fe97c51e29c89bd07b50fba506a256
bb4L <39266013+bb4L@users.noreply.github.com>
2020-06-29 19:33:34 +0200

    Add tool to commit specific solution (#10)

---
+++ src/co.nim
@@ -0,0 +1,27 @@
+import os, strutils,  terminal
+
+when isMainModule:
+    if paramCount() != 1:
+        styledEcho fgRed, "Exactly 1 argument is needed"
+        quit(0)
+    
+    else:
+        let s = paramStr(1)
+        if len(s)!=3:
+            styledEcho fgRed, "Argument has to be 3-digits, with zero padding"
+            quit(0)
+        else:
+            for c in s:
+                if not isDigit(c):
+                    styledEcho fgRed, "Argument is not digits only"
+            
+            discard execShellCmd("git add " & s)
+            let res =  execShellCmd("git co -b feat/" & s)
+            if res == 0:
+                discard execShellCmd("git cim \"Add solution for " & s & "\"")
+                discard execShellCmd("git push -u origin feat/" & s)
+
+
+            # let k = parseInt(s)
+            # echo "K"
+            # echo k
